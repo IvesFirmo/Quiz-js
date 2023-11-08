b0ab89f4f3c710549d2a9ff0017f6bde83933915
Ives Firmo <ivesfirmo@live.com>
2023-11-08 06:53:11 -0300

    Quiz js

---
+++ Js/questions.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        question: "Quanto é 12 + 20?",
+        answers: [
+            { option: "32", correct: true },
+            { option: "2", correct: false },
+            { option: "33", correct: false },
+        ],
+    },
+    {
+        question: "Quanto é 12 + 24?",
+        answers: [
+            { option: "36", correct: true },
+            { option: "7", correct: false },
+            { option: "37", correct: false },
+        ],
+    },
+    {
+        question: "Quanto é  5 x 4?",
+        answers: [
+            { option: "32", correct: false },
+            { option: "25", correct: false },
+            { option: "20", correct: true },
+        ],
+    },
+     {
+        question: "Quanto é 6 x 30?",
+        answers: [
+            { option: "32", correct: false },
+            { option: "180", correct: true },
+            { option: "160", correct: false },
+        ],
+    },
+];
+
